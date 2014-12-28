6612b3c21b74c4e46ceacbac04b17ebf94c4d7ae
Bradley Smith <bradleydsmith@gmail.com>
2014-12-27 20:52:13 -0700

    add door module

--- lib/portal/door.ex
+++ lib/portal/door.ex
@@ -16,4 +16,23 @@ defmodule Portal.Door do
     Agent.get(door, fn list -> list end)
   end
 
+  @doc """
+  Pushes `value` into door
+  """
+    def push(door, value) do
+      Agent.update(door, fn list -> [value|list] end)
+  end
+
+  @doc """
+  Pop's a `value` from a door
+  Returns `{:ok, value}` if there is a value
+    or `:error` if the hole is currently empty.
+  """
+  def pop(door) do
+    Agent.get_and_update(door, fn
+      [] -> {:error, []}
+      [h|t] -> {{:ok, h}, t} 
+    end)
+  end
+
 end
